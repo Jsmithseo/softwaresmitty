bd0c33a64d9426268b9e92b598705832d1616497
Admin <admin@Addapps-MacBook-Pro.local>
2021-01-28 18:40:57 -0800

    WIP

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import react from "react"
+
+
+
+const Index = () => {
+    return(
+        <>
+        <h1>Hello Welcome to Software Smitty world</h1>
+        </>
+    )
+}
+
+
+
+
+export default Index
